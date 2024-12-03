cb78ce2ed40cd121e23e7961f6427c548707ced5
Miapaysse <162704754+Miapaysse@users.noreply.github.com>
2024-12-03 02:22:50 -0300

    Update PRUEBA_MPU6050.ino (Saco cosas innecesarias)

--- mpus/PRUEBA_MPU6050/PRUEBA_MPU6050.ino
+++ mpus/PRUEBA_MPU6050/PRUEBA_MPU6050.ino
@@ -12,7 +12,7 @@ void setup(void) {
   Serial.println("Adafruit MPU6050 test!");
 
   // Try to initialize!
-  if (!mpu.begin()) {
+  if (!mpu.begin(0x68)) {
     Serial.println("Failed to find MPU6050 chip");
     while (1) {
       delay(10);
